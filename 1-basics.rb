ca2f092883e26aafae793404866f0cf481d23bbc
mjmeilahn <mjmeilahn@gmail.com>
2020-03-19 09:22:26 -0700

    Intro to numerical and alphabetical ranges

--- 1-basics.rb
+++ 1-basics.rb
@@ -9,14 +9,25 @@
 # puts input.to_i * 2
 
 # 3. HOMEWORK
-puts "Enter your first name:"
-first = gets.chomp
-first_len = first.length
+# puts "Enter your first name:"
+# first = gets.chomp
+# first_len = first.length
 
-puts "Enter your last name:"
-last = gets.chomp
-last_len = last.length
+# puts "Enter your last name:"
+# last = gets.chomp
+# last_len = last.length
 
-puts "Your full name is: #{first} #{last}"
-puts "Your full name reversed is: #{first.reverse} #{last.reverse}"
-puts "Your name has #{first_len + last_len} characters in it"
+# puts "Your full name is: #{first} #{last}"
+# puts "Your full name reversed is: #{first.reverse} #{last.reverse}"
+# puts "Your name has #{first_len + last_len} characters in it"
+
+# 4. RANGES & ARRAY CONVERSION
+# abc = "a".."z"
+# p abc.to_a
+
+# x = 1..100
+# y = x.to_a
+# z = y.shuffle
+# puts x.class
+# p y
+# p z

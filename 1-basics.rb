b4fd625a45b02d8b3cf3a30ca5783e87679709c1
mjmeilahn <mjmeilahn@gmail.com>
2020-03-19 08:07:00 -0700

    Complete homework

--- 1-basics.rb
+++ 1-basics.rb
@@ -1,7 +1,22 @@
+# 1. ARRAYS
 # arr = [1, 2, 3, 4, 5]
 # puts arr
 # puts arr.reverse
 
-puts 'Multiply any number by 2'
-input = gets.chomp
-puts input.to_i * 2
+# 2. CLI INPUT
+# puts 'Multiply any number by 2'
+# input = gets.chomp
+# puts input.to_i * 2
+
+# 3. HOMEWORK
+puts "Enter your first name:"
+first = gets.chomp
+first_len = first.length
+
+puts "Enter your last name:"
+last = gets.chomp
+last_len = last.length
+
+puts "Your full name is: #{first} #{last}"
+puts "Your full name reversed is: #{first.reverse} #{last.reverse}"
+puts "Your name has #{first_len + last_len} characters in it"
